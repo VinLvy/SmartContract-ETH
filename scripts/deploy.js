74c7affdfa2549e510729a52c97df5e924d83708
Vinluvy <123059312+VinLvy@users.noreply.github.com>
2025-05-09 13:00:16 +0700

    1.2

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,17 @@
 const hre = require("hardhat");
 
 async function main() {
-  const unlockTime = Math.floor(Date.now() / 1000) + 60; 
+  const unlockTime = Math.floor(Date.now() / 1000) + 60;
   const lockedAmount = hre.ethers.parseEther("0.01");
 
-  const Lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  // Deploy kontrak
+  const Lock = await hre.ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-  await Lock.waitForDeployment();
+  await lock.waitForDeployment();
 
-  console.log(`Lock deployed to: ${Lock.target}`);
+  console.log(`Lock deployed to: ${lock.target}`); // .target untuk ethers v6
 }
-
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
