c75dd09d61184c8ed401b44158244b3ad80e59b6
Vinluvy <123059312+VinLvy@users.noreply.github.com>
2025-06-07 13:43:07 +0700

    1.3

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,5 @@
 const hre = require("hardhat");
+const fs = require("fs");
 
 async function main() {
   const unlockTime = Math.floor(Date.now() / 1000) + 60;
@@ -10,8 +11,19 @@ async function main() {
 
   await lock.waitForDeployment();
 
-  console.log(`Lock deployed to: ${lock.target}`); // .target untuk ethers v6
+  console.log(`Lock deployed to: ${lock.target}`);
+
+  // Simpan alamat kontrak ke file JSON
+  const addressData = {
+    lockAddress: lock.target,
+  };
+
+  fs.writeFileSync(
+    "contractAddress.json",
+    JSON.stringify(addressData, null, 2)
+  );
 }
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
